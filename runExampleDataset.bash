e521960f2f2ed309f4369eca45fe0ec6eeaae92b
Steven Shave <stevenshave@gmail.com>
2017-04-24 10:32:05 +0100

    Updated runExampleDataset script

--- runExampleDataset.bash
+++ runExampleDataset.bash
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-
+if [ ! -f pulse ]; then
+    echo -e "pulse not found, please compile it first with \"./configure && make\".\nWith the pulse executable in this directory, this script may be used to run the included example."
+    exit
+fi
 
 ./pulse sample-pulse-5merCyclic-CGTTGCXXXXXXXXXXXXXXXTGTGCT.fastq.gz CGTTGCXXXXXXXXXXXXXXXTGTGCT UAG Q
 
